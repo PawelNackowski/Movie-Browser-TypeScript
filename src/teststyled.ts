b9621ff61570d7eb2b5f05407e2cfa327e27e391
Pawel Nackowski <pawelnackowski@gmail.com>
2024-03-28 15:36:51 +0100

    deleted code to app.tsx and created test component

---
+++ src/teststyled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const TestComponent = styled.div`
+  color: white;
+  font-size: 20px;
+  background-color: gray;
+  text-align: center;
+  padding: 10px;
+  border-radius: 5px;
+  font-family: Poppins;
+  font-weight: 500;
+`;
