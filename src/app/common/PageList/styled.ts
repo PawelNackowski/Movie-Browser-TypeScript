242befd2a28e595ee0e523dba51f0d0e1444605e
Pawel Nackowski <pawelnackowski@gmail.com>
2024-04-09 10:06:19 +0200

    added styled to header and GridList

--- src/app/common/PageList/styled.ts
+++ src/app/common/PageList/styled.ts
@@ -1,4 +1,14 @@
 import styled from "styled-components";
 
 export const Header = styled.div`
+  font-size: 36px;
+  font-weight: ${({ theme }) => theme.fontsWeight.bold};
+  margin: 56px 0 24px 0;
+`;
+
+export const GridList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(285px, 1fr));
+  gap: 24px;
+  margin-bottom: 40px;
 `;
