efb976d500218135f98e6a2204328e1bc7799c82
Pawel Nackowski <pawelnackowski@gmail.com>
2024-04-09 10:07:34 +0200

    added testData tile to see the view without API

---
+++ src/features/pages/testData.tsx
@@ -0,0 +1,60 @@
+
+export const tileData = [
+    {
+      title: "Mulan",
+      description: "2020",
+      tag: ["tag1", "tag2", "tag3"],
+      rate: 6.7,
+      vote: 56,
+    },
+    {
+      title: "Mulan",
+      description: "2020",
+      tag: ["tag1", "tag2", "tag3"],
+      rate: 6.7,
+      vote: 56,
+    },
+    {
+      title: "Mulan",
+      description: "2020",
+      tag: ["tag1", "tag2", "tag3"],
+      rate: 6.7,
+      vote: 56,
+    },
+    {
+      title: "Mulan",
+      description: "2020",
+      tag: ["tag1", "tag2", "tag3"],
+      rate: 6.7,
+      vote: 56,
+    },
+    {
+      title: "Mulan",
+      description: "2020",
+      tag: ["tag1", "tag2", "tag3", "tag3", "tag3", "tag3", "tag3"],
+      rate: 6.7,
+      vote: 56,
+    },
+    {
+      title: "Mulan",
+      description: "2020",
+      tag: ["tag1", "tag2", "tag3"],
+      rate: 6.7,
+      vote: 56,
+    },
+    {
+      title: "Mulan",
+      description: "2020",
+      tag: ["tag1", "tag2", "tag3"],
+      rate: 6.7,
+      vote: 56,
+    },
+    {
+      title: "Mulan",
+      description: "2020",
+      tag: ["tag1", "tag2", "tag3"],
+      rate: 6.7,
+      vote: 56,
+    },
+  ];
+  
